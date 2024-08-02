dd65406ba983addcbb936496eccf630684eff692
lordeadroid <rishabhrawat34@gmail.com>
2024-08-02 07:57:34 +0530

    feat: requesting data by time and limiting no. of tweets to fetch

--- src/services/get-tweets.ts
+++ src/services/get-tweets.ts
@@ -1,9 +1,15 @@
 import { DB } from "../utils/constant";
 import { TTweet } from "../utils/types";
-import db, { collection, getDocs } from "./db";
+import db, { collection, getDocs, limit, orderBy, query } from "./db";
 
-const getTweets = async (): Promise<TTweet[]> => {
-  const queryData = await getDocs(collection(db, DB.tweets));
+const getTweets = async (queryLimit = 10): Promise<TTweet[]> => {
+  const tweetsQuery = query(
+    collection(db, DB.tweets),
+    orderBy("timestamp", "desc"),
+    limit(queryLimit),
+  );
+
+  const queryData = await getDocs(tweetsQuery);
   const tweetsData = queryData.docs.map((doc) => doc.data() as TTweet);
 
   return tweetsData;
