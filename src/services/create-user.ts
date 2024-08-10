1c3500fb2daff5733cdb0c7e55b3039ed8ee0a9d
lordeadroid <rishabhrawat34@gmail.com>
2024-08-10 22:11:57 +0530

    fix: added missing function

---
+++ src/services/create-user.ts
@@ -0,0 +1,17 @@
+import db, { collection, addDoc } from "./db";
+
+const addUser = async (UID: string, username: string, message: string) => {
+  try {
+    await addDoc(collection(db, "users"), {
+      UID,
+      username,
+      message,
+      timestamp: Date.now(),
+    });
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error("Error adding document: ", error);
+  }
+};
+
+export default addUser;
