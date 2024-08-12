e052b357eabbc1046d8b954ddb63a7935eb8e31a
lordeadroid <rishabhrawat34@gmail.com>
2024-08-12 16:57:10 +0530

    chore: added mantine notification package

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import "@mantine/notifications/styles.css";
 import "./index.css";
-import { MantineProvider } from "@mantine/core";
 import theme from "./theme.ts";
+import App from "./App.tsx";
+import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <MantineProvider theme={theme}>
+    <Notifications />
     <App />
-  </MantineProvider>
+  </MantineProvider>,
 );
