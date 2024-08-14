bab7ffb182db06e847e27b49bc12d10258d404fe
lordeadroid <rishabhrawat34@gmail.com>
2024-08-14 15:45:09 +0530

    feat: showing all users in explore section

--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
 import useLoginStore from "../../context/use-login-store";
 import { Avatar, Table, Text, Flex, Button } from "@mantine/core";
@@ -85,10 +84,6 @@ const ExplorePage = () => {
       </Table.ScrollContainer>
     </Page>
   );
-=======
-const ExplorePage = () => {
-  return <h1>explore</h1>;
->>>>>>> a3cd216 (fix: added mising file)
 };
 
 export default ExplorePage;
