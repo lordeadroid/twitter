7e8b2d81cce6e9c845e5a4af9031e519ab35687e
lordeadroid <rishabhrawat34@gmail.com>
2024-08-14 15:45:09 +0530

    feat: showing all users in explore section

--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react";
+import getUserDetails, { TUser } from "../../services/get-user-details";
+import { Avatar, Table, Text, Flex, Button } from "@mantine/core";
+
 const ExplorePage = () => {
-  return <h1>explore</h1>;
+  const [users, setUsers] = useState<TUser[] | null>(null);
+
+  useEffect(() => {
+    getUserDetails(null).then((users: TUser[]) => {
+      setUsers(users);
+    });
+  }, []);
+
+  return (
+    <Table.ScrollContainer minWidth={800} w="100%" p="xl">
+      <Table verticalSpacing="lg">
+        <Table.Tbody>
+          {users?.map((element) => (
+            <Table.Tr key={element.UID}>
+              <Table.Td>
+                <Flex align="center" justify="space-between">
+                  <Flex align="center" gap="xl">
+                    <Avatar
+                      size={60}
+                      src={element.images.avatar}
+                      radius="25%"
+                    />
+                    <Text fz="xl" fw={500}>
+                      {element.username}
+                    </Text>
+                  </Flex>
+                  <Flex gap="md">
+                    <Button>Add Friend</Button>
+                    <Button color="red">Block</Button>
+                  </Flex>
+                </Flex>
+              </Table.Td>
+            </Table.Tr>
+          ))}
+        </Table.Tbody>
+      </Table>
+    </Table.ScrollContainer>
+  );
 };
 
 export default ExplorePage;
