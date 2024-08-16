6dfe6029f4a35bee2d415c9b252c738fc4e116a0
lordeadroid <rishabhrawat34@gmail.com>
2024-08-16 16:41:00 +0530

    refact: styling of page + router change

--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -6,10 +6,7 @@ import Requests from "../../components/requests";
 import { addFriend } from "../../services/requests";
 import { EMPTYSTRING, PATH } from "../../utils/constant";
 import { useNavigate } from "react-router-dom";
-<<<<<<< HEAD
 import Page from "../../components/Page";
-=======
->>>>>>> 29fa13e (feat: updating approve section onclick)
 
 const UserProfile = ({
   userData,
